436c1aae940903769b5f60d51733197a9b21d070
Jared Grove <jaredkgrove@gmail.com>
2019-12-05 12:39:43 -0600

    removed material-ui dependency in HomeView. Added styled components

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,25 +1,13 @@
 import React, {useEffect}  from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import InstagramFeed from "../containers/InstagramFeed";
 import WelcomeHeader from "../components/WelcomeHeader";
 import ProductsView from "./ProductsView";
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components'
 
-const useStyles = makeStyles(theme => ({
-  home: {
-    backgroundColor: theme.palette.primary.grey,
-    // height:'100vh',
-    top:'0px',
-    position:'absolute',
-    overflow: 'hidden',
-    width: '100vw'
-  },
-
-}));
 
 const HomeView = () => {
   const [currentView, setCurrentView] = React.useState(0);
-  const classes = useStyles();
   let history = useHistory();
 
   const handleScroll = (e) => {
@@ -32,17 +20,7 @@ const HomeView = () => {
 
   const nextView = () => {
     if (currentView < 1){
-      // setCurrentView(currentView + 1) 
-      // browserHistory.push({
-      //   pathname: '/products'
-      //  });\
-      // console.log(props)
-      
        history.push({pathname:'/products'})
-      // history.go(1)
-      // history.go(1)
-
-      // window.history.pushState('', '', '/products')
     }
   }
 
@@ -53,15 +31,21 @@ const HomeView = () => {
     }
   }
     return(
-        <div className={classes.home} onWheel={handleScroll}>
+        <Home onWheel={handleScroll}>
           <WelcomeHeader visible={true}/>
           {/* <ProductsView visible={currentView === 1}/> */}
 
-          <InstagramFeed visible={true}/>
-        </div>
+          {/* <InstagramFeed visible={true}/> */}
+        </Home>
     )
 }
 
 export default HomeView
 
 
+const Home = styled.div`
+  top: 0px;
+  position: absolute;
+  overflow: auto;
+  width: 100vw;
+`;
